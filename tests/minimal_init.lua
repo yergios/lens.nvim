bad14cc5916b23ec49cecdf040422ccda517fda5
Sergio Yunsuk Lee <sergioleesp@gmail.com>
2026-05-01 20:59:08 +0200

    persistent plenary cache clone

--- tests/minimal_init.lua
+++ tests/minimal_init.lua
@@ -1,8 +1,7 @@
--- Bootstrap plenary from lazy.nvim's install location, falling back to a clone
 local plenary_path = vim.fn.stdpath("data") .. "/lazy/plenary.nvim"
 
 if vim.fn.isdirectory(plenary_path) == 0 then
-  plenary_path = "/tmp/plenary.nvim"
+  plenary_path = vim.fn.stdpath("cache") .. "/lens-test/plenary.nvim"
   if vim.fn.isdirectory(plenary_path) == 0 then
     vim.fn.system({
       "git",
